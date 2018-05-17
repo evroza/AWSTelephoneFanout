31079bfeb467c22d4e56ceac7b03d4aad1cbfa8d
Evans R <evansrutoh@gmail.com>
2018-05-18 04:57:41 +0800

    - Registering new Telephone devices added - Moved data storage from JavaScript objects to DynamoDB

--- functions/login.js
+++ functions/login.js
@@ -15,31 +15,55 @@ const JWT_EXPIRATION_TIME = '5m';
   */
 module.exports.handler = (event, context, callback) => {
   console.log('login');
-  const { username, password } = JSON.parse(event.body);
-
   try {
-    // Authenticate user
-    //const user = users.login(username, password);
-	// Check in dynamoDB if the user exists and password correct
-	
-    console.log(user);
-
-    // Issue JWT
-    const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: JWT_EXPIRATION_TIME });
-    console.log(`JWT issued: ${token}`);
-    const response = { // Success response
-      statusCode: 200,
+  var { username, password } = JSON.parse(event.body);
+  } catch(err){
+	  console.error("Post data not correct - Check your request and try again");
+	  const response = { // Error response
+      statusCode: 401,
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify({
-        token,
+        error: err.message,
+		moreInfo: "Post data not correct - Check your request and try again"
       }),
     };
-
-    // Return response
-    console.log(response);
     callback(null, response);
+  }
+
+  try {
+    // Authenticate telephone
+	// Check in dynamoDB if the telephone exists and password correct
+    const telephone = users.login(username, password);	
+	
+    console.log(telephone);
+
+	if(telephone){
+		console.log("Telephone successfully logged in");
+		// Issue JWT
+		const token = jwt.sign({ telephone }, process.env.JWT_SECRET, { expiresIn: JWT_EXPIRATION_TIME });
+		console.log(`JWT issued: ${token}`);
+		const response = { // Success response
+		  statusCode: 200,
+		  headers: {
+			'Access-Control-Allow-Origin': '*',
+		  },
+		  body: JSON.stringify({
+			token,
+		  }),
+		};
+
+		// Return response
+		console.log(response);
+		callback(null, response);																									
+	}
+	else {
+		console.error("Telephone with provided details couldn't be found!");
+		callback(new Error("Telephone with provided details couldn't be found!"));	
+		return; 
+	}
+    
   } catch (e) {
     console.log(`Error logging in: ${e.message}`);
     const response = { // Error response
